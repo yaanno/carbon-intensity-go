ffc27d7845e7509a850a666802e4e0a4ea0e4eed
Janos Hardi <janos-karoly.hardi@telekom.com>
2024-07-18 18:28:15 +0200

    add subcommands to national

--- cmd/national_bydate.go
+++ cmd/national_bydate.go
@@ -4,12 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-// nationalCmd represents the national command
-var nationalCmd = &cobra.Command{
-	Use:   "national",
+// natCmd represents the nat command
+var nationalByDateCmd = &cobra.Command{
+	Use:   "date",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -18,20 +20,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Usage()
+		fmt.Println("nat called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(nationalCmd)
+	nationalCmd.AddCommand(nationalByDateCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// nationalCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// natCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// nationalCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// natCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
